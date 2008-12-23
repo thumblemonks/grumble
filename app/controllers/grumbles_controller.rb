0798c57a71585accf95686bc743f24efe5ebdf8c
Gabriel Gironda <gabriel.gironda@gmail.com>
2008-12-22 21:25:43 -0600

    unix dates plz

--- app/controllers/grumbles_controller.rb
+++ app/controllers/grumbles_controller.rb
@@ -18,7 +18,7 @@ private
   def grumble_attributes(grumble)
     grumble_data = {:id => grumble.uuid, :grumble_url => grumble_url(:id => grumble, :target_id => grumble.target), 
                     :grumbler_name => grumble.grumbler_name, :subject => grumble.subject, :body => grumble.body, 
-                    :created_at => grumble.created_at}
+                    :created_at => grumble.created_at.to_i}
     grumble_data[:grumbler_url] = grumbler_url(grumble.grumbler) if grumble.grumbler    
     grumble_data
   end

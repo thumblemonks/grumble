a161d8ce7c28294cb7c4725203cf18545bdbc3b4
Justin Knowlden <gus@gusg.us>
2009-10-12 22:49:07 -0500

    renaming database output and ignoring it

--- config/environment.rb
+++ config/environment.rb
@@ -32,6 +32,7 @@ Rails::Initializer.run do |config|
   config.gem 'mocha'
   config.gem 'haml'
   config.gem 'uuidtools'
+
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named

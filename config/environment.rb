d90b3cc4a2b891b9c04ccd7e3a63bc223eb64960
Justin Knowlden <gus@gusg.us>
2009-10-12 23:04:47 -0500

    Adding test gems to test.rb

--- config/environment.rb
+++ config/environment.rb
@@ -27,9 +27,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'thoughtbot-shoulda',    :lib => 'shoulda/rails', :source => 'http://gems.github.com'
-  config.gem "gabrielg-factory_girl", :lib => "factory_girl",  :source => "http://gems.github.com"
-  config.gem 'mocha'
   config.gem 'haml'
   config.gem 'uuidtools'
 
